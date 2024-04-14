65687050a39e555a50059f58d90cdbb087866449
Mher Goorgenian <40806241+mhergoorgenian@users.noreply.github.com>
2024-04-14 20:01:02 +0330

    Create QrGenerator.py

---
+++ QrGenerator.py
@@ -0,0 +1,20 @@
+import qrcode
+
+def generate_qr_code(url, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+if __name__ == "__main__":
+    url = "yourUrl"
+    filename = r"C:\Users\Mg\Desktop\pyproject\Qrcode\test.png"
+    generate_qr_code(url, filename)
+    print(f"QR Code saved as {filename}")
